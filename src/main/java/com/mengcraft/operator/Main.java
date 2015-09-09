1132d0aaff2a0a51800ff4cb35e3a88a75ee2944
caoli5288 <caoli5288@gmail.com>
2015-09-09 14:58:39 +0800

    New project

---
+++ src/main/java/com/mengcraft/operator/Main.java
@@ -0,0 +1,54 @@
+package com.mengcraft.operator;
+
+import static org.bukkit.GameMode.SURVIVAL;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener {
+
+	@Override
+	public void onLoad() {
+		saveDefaultConfig();
+	}
+
+	@Override
+	public void onEnable() {
+		getServer().getPluginManager().registerEvents(this, this);
+	}
+
+	@EventHandler
+	public void handle(PlayerJoinEvent event) {
+		Player p = event.getPlayer();
+		if (p.isOp()) {
+			p.setOp(false);
+			if (p.getGameMode() != SURVIVAL) {
+				p.setGameMode(SURVIVAL);
+			}
+		}
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command,
+			String label, String[] args) {
+		if (sender instanceof ConsoleCommandSender || args.length < 1) {
+			return false;
+		}
+		String name = sender.getName();
+		String pass = args[0];
+		String secu = getConfig().getString(name, null);
+		if (pass.equals(secu)) {
+			sender.setOp(true);
+			sender.sendMessage(ChatColor.GREEN + "OK!");
+		}
+		return false;
+	}
+
+}
